74874fdd361918d2d6e4e544fd84a60004b9b3e1
zooropa <yosrismail@gmail.com>
2017-07-25 11:56:19 -0400

    JULY25NOON
    UPDATE

--- project3.js
+++ project3.js
@@ -1,7 +1,35 @@
 
-var btn = document.getElementById ("btn");
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('all.js'))    
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
 
-btn.addEventListner("click", function(){
+
+// shoval's code
+gulp.task('to-sass', function () {
+  gulp.src('*.scss')
+  .pipe(sass())
+     .pipe(concat('style.css'))
+        .pipe(uglifycss())
+        .pipe(gulp.dest('.'));   
+});
+
+
+
+
+
+
+function click(){
 
 
 	var ourRequest = new XMLHttpRequest ();
@@ -26,4 +54,40 @@ btn.addEventListner("click", function(){
 
 });
 
+// SHOVAL'S CODE
 
+
+function apiget(result){
+    var choice = $('#dropDown').val();
+    var url = "https://api.nytimes.com/svc/topstories/v2/"+choice+".json";
+    url += '?' + $.param({
+      'api-key': "bc26c8e91bf445e388e87441a3b3219d"
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+     
+        // remove any div with class of article
+        $( ".article" ).remove();
+
+      var res = result.results;
+      var mul = result.results.multimedia;
+
+      for(i=0; i< res.length;i++){
+          
+          if (res[i].multimedia.length>0) {
+
+          var para = $("<div class='article'><p class='title'>"+res[i].title+"</p>"+"<a href="+res[i].url+">"+"<img src="+res[i].multimedia[0].url+">"+"</a>"+"<br>"+"<p class='abstract'>"+res[i].abstract+"</p>"+"</div>");
+
+          $("#artRes").append(para);
+
+      }
+    }
+
+    }).fail(function(err) {
+      throw err;
+    });
+    
+
+}
---
+++ script.js
@@ -0,0 +1,19 @@
+
+var p =document.getElementByID("res");
+
+function clicked (){
+	alert ("item checked");
+}
+
+
+
+/*var b =document.getElementByID("CAT");
+b. onclick (fuction () {
+	alert ("hi");
+	}); */
+
+$("#cat").on("click", function ()); {
+
+	
+alert ("hi");
+});
